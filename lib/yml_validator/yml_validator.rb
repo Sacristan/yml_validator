7a712d382b143255c90fc994f27531cb131246ff
Sacristan <girts.kesteris@gmail.com>
2015-01-14 11:17:53 +0200

    Added Global namespace valid? method

--- lib/yml_validator/yml_validator.rb
+++ lib/yml_validator/yml_validator.rb
@@ -5,6 +5,10 @@ module YMLValidator
     def initialize file_path
       @file_path = file_path
     end
+    
+    def self.valid? file_path
+      YMLValidator.new(file_path).valid?
+    end
 
     def valid?
       begin
@@ -15,4 +19,7 @@ module YMLValidator
       end
     end
   end
+
+
+
 end
