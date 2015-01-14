23b43bed5ea969caabda306ce7ae570c2683f0cb
Sacristan <girts.kesteris@gmail.com>
2015-01-14 12:42:00 +0200

    Refactored - Helpers moved to YMLValidator::Helpers to avoid conflicts with toplevel namespace

--- lib/yml_validator.rb
+++ lib/yml_validator.rb
@@ -3,4 +3,13 @@ require 'yml_validator/base'
 require 'yml_validator/helpers'
 require 'yml_validator/version'
 
-YMLValidator = YMLValidator::Base
+module YMLValidator
+  def self.new file_path
+    YMLValidator::Base.new file_path
+  end
+
+  def self.valid? file_path
+    YMLValidator.new(file_path).valid?
+  end
+
+end
--- lib/yml_validator/base.rb
+++ lib/yml_validator/base.rb
@@ -1,16 +1,11 @@
 module YMLValidator
   class Base
-
     attr_accessor :file_path
     attr_accessor :file_to_validate
     attr_accessor :normalized_file
 
     def initialize file_path
       @file_path = file_path
-    end
-
-    def self.valid? file_path
-      YMLValidator.new(file_path).valid?
     end
 
     def valid?
@@ -25,7 +20,7 @@ module YMLValidator
 
     private
       def validate
-        @normalized_file = Helpers.normalize_yaml @file_to_validate
+        @normalized_file = YMLValidator::Helpers.normalize_yaml @file_to_validate
         @normalized_file.is_a?(Hash) && !!@normalized_file.to_yaml
       end
   end
